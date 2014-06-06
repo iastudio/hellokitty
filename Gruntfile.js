18e5f05b8e3ddb4ac4dd26c2b1800134f9c4b3e6
Ivan Rudoy - Work <rudooy@gmail.com>
2014-06-06 18:22:38 +1100

    init

---
+++ Gruntfile.js
@@ -0,0 +1,92 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    autoprefixer: {
+        dist: {
+            files: {
+                'css/app.css': 'css/app.css'
+            }
+        }
+    },
+    concat: {
+      options: {
+        stripBanners: true,
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+          '<%= grunt.template.today("dd-mm-yyyy") %> */',
+      },
+      dist: {
+        src: ['js/jquery.placeholder.js', 'js/jquery.validate.min.js', 'js/lightbox-2.6.min.js', 'js/jquery.easing.1.3.js', 'js/jquery.modal.min.js', 'js/jquery-ui-1.10.4.custom.min.js', 'js/jquery.flexverticalcenter.js', 'js/rangeSlider.min.js', 'js/app.js'],
+        dest: 'js/built.js',
+      },
+    },
+    uglify: {
+      options: {
+        // compress: {
+        //   global_defs: {
+        //     "DEBUG": false
+        //   },
+        //   dead_code: true
+        // },
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'js/app.min.js': ['js/built.js']
+        }
+      }
+    },
+    jade: {
+      compile: {
+        files: [{
+          cwd: 'jade',
+          src: ['*.jade'],
+          dest: '.',
+          expand: true,
+          ext: '.html'
+        }]
+      },
+      options: {
+        pretty: true,
+      }
+    },
+    compass: {
+      dist: {
+        options: {
+          config: 'config.rb'
+        }
+      }
+    },
+    watch: {
+      grunt: { files: ['Gruntfile.js'] },
+
+      compass: {
+        files: 'scss/**/*.scss',
+        tasks: ['compass', 'autoprefixer']
+      },
+      jade: {
+        files: ['jade/**/*.jade'],
+        tasks: ['jade'],
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          hostname: '127.0.0.1',
+          port: 3000,
+          dest: '.'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('build', ['compass']);
+  grunt.registerTask('default', ['connect', 'build','watch','jade','concat','uglify']);
+}
